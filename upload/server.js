d488e01b69cc78ff78cccfff25060212d8263787
IMRANDIL <aliimranadil2@gmail.com>
2023-09-16 19:36:20 +0530

    added some more change

--- upload/server.js
+++ upload/server.js
@@ -1,23 +1,21 @@
-const net = require('net');
+const net = require("net");
+const fs = require("fs/promises");
+const server = net.createServer(() => {});
 
-const server = net.createServer(()=>{
+server.on("connection", (socket) => {
+  console.log("new connection");
 
+  socket.on("data", async (data) => {
+    const fileHandle = await fs.open(`storage/upload.txt`, "w");
+    const fileWriteStream = fileHandle.createWriteStream();
+    if (!fileWriteStream.write(data)) {
+      socket.pause();
+    }
+    socket.resume();
+    fileWriteStream.write(data);
+  });
 });
 
-server.on('connection', (socket)=>{
-    console.log('new connection')
-    socket.on('data', (data)=>{
-        console.log(data.toString('utf-8'));
-        socket.write(data)
-    })
-})
-
-
-
-
-
-
-
-server.listen(7000, "127.0.0.1", ()=>{
-    console.log(`upload server started on:`, server.address())
-})
+server.listen(7000, "127.0.0.1", () => {
+  console.log(`upload server started on:`, server.address());
+});
