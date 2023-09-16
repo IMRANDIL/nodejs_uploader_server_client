bd2dd3ef66a9c9e5394a854d908450c7789bc412
IMRANDIL <aliimranadil2@gmail.com>
2023-09-16 19:27:36 +0530

    some more change

--- upload/server.js
+++ upload/server.js
@@ -1,8 +1,11 @@
 const net = require('net');
 
-const server = net.createServer();
+const server = net.createServer(()=>{
+
+});
 
 server.on('connection', (socket)=>{
+    console.log('new connection')
     socket.on('data', (data)=>{
         console.log(data.toString('utf-8'));
         socket.write(data)
